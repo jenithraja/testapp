a670b240a3e7a25bcb0fd301b5cc233a9be2b790
Sundar <itssundar05@yahoo.co.in>
2016-03-17 18:06:18 +0530

    sundar

--- testapp/AppDelegate.h
+++ testapp/AppDelegate.h
@@ -4,8 +4,7 @@
 //
 //  Created by DTM002 on 12/08/15.
 //  Copyright (c) 2015 DTM002. All rights reserved.
-//jenith sundar sdfsfsdfs df
-
+//jenith sundar sdfsfsdfs df sundar
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
